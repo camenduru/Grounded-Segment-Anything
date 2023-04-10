9a3dd4fb8adcd64c06a6a97ec52d7f5c3946c943
camenduru <54370274+camenduru@users.noreply.github.com>
2023-04-10 18:17:06 +0300

    value="seg"

--- gradio_app.py
+++ gradio_app.py
@@ -274,7 +274,7 @@ if __name__ == "__main__":
             with gr.Column():
                 input_image = gr.Image(source='upload', type="pil")
                 text_prompt = gr.Textbox(label="Detection Prompt")
-                task_type = gr.Dropdown(["det", "seg", "inpainting"], label="task type: det/seg/inpainting")
+                task_type = gr.Dropdown(["det", "seg", "inpainting"], label="task type: det/seg/inpainting", value="seg")
                 inpaint_prompt = gr.Textbox(label="Inpaint Prompt")
                 run_button = gr.Button(label="Run")
                 with gr.Accordion("Advanced options", open=False):
